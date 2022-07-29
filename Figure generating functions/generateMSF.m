41ae2dd1d0fe3d8fa5231232adbbd640396e6509
yoshikisd <yoshikisd@yoshikisd-X299-MICRO>
2022-07-29 12:04:18 -0700

    1) Added new MSF calculation method 2) Changed generateMSF function inputs 3) Included module for tracking ASI evolution in ASIWizard UI (non functional at the moment)

--- Figure generating functions/generateMSF.m
+++ Figure generating functions/generateMSF.m
@@ -1,61 +1,64 @@
 % Generates and plots the magnetic structure factor
 %function generateMSF(app,savePath,saveOption,tableOption)
-function [MSF,MSFTable] = generateMSF(app,steps,qMax,xSpin,ySpin,xR,yR,options)
+function [MSF,MSFTable] = generateMSF(app,steps,qMax,xSpin,ySpin,xR,yR,method,type,options)
     arguments
-        app
-        steps (1,1) int16
-        qMax (1,1) double
-        xSpin double
-        ySpin double
-        xR double
-        yR double
-        options.SaveTo char % Combines savePath and saveOption
+        app                     % The ASIWizard app. Set this parameter to ~ when not using the app
+        steps (1,1) int16       % Number of pixels along qx/qy
+        qMax (1,1) double       % Max/min values of qx/qy
+        xSpin (:,1) double      % Vector containing x component of spin vector
+        ySpin (:,1) double      % Vector containing y component of spin vector
+        xR (:,1) double         % Vector containing x position of position vector
+        yR (:,1) double         % Vector containing y position of position vector
+
+        % MSF calculation method
+        %   "full": Calculate each individual point
+        %   "half": Calculate only the 1st and 4th quadrants in q-space. Then
+        %           use inversion symmetry relation to extract 2nd and 3rd quadrants
+        method {mustBeMember(method,['full','half'])}
+
+        % Type of dataset
+        %   "ideal asi": ASI data extracted from ASIWizard or IceScanner
+        %   "mumax': Data extracted from mumax/oommf ovf file converted into csv
+        type {mustBeMember(type,['asi','mumax'])}
+        
+        % Directory to save MSF data as an image or (optionally) a table
+        options.SaveTo char
+
+        % Option to save MSF data as a table. SaveTo must be populated
         options.Table {mustBeMember(options.Table,['on','off'])}
-        options.Type {mustBeMember(options.Type,["ideal asi","mumax"])}
     end
 
     try
-        range_qx = linspace(-qMax*2*pi,qMax*2*pi,steps);
-        range_qy = range_qx;
-        MSF = zeros(length(range_qx));
-        MSFTable = zeros(length(MSF)^2,3);
-        % Remove all NaN magnets
-        xR = xR(~isnan(xSpin));         yR = yR(~isnan(xSpin));
-        xSpin = xSpin(~isnan(xSpin));   ySpin = ySpin(~isnan(xSpin));
-        invSpinLength = 1/length(xSpin);
-        onesVector_Q = ones(steps,1);
+        % Define qx and qy positions
+        range_qx = linspace(-qMax*2*pi,qMax*2*pi,steps); range_qy = range_qx;
+        % Define the qx range for the 1st/4th quadrant
+        qx_Q1_Range = range_qx(steps/2+1:end);
+        % Define the MSF in "2D image" and table form
+        MSF = zeros(length(range_qx));      % Dimensions: row = qy, column = qx
+        MSFTable = zeros(length(MSF)^2,3);  % Dimensions: col1: qx, col2: qy, col3: I
+        % Create doubleSteps. It's the double-precision version of steps
+        doubleSteps = double(steps);
 
-        %% Calculating MSF
-        % If ASIWizard is calling this function, create the loading bar in the UI and update the loading counter after
-        % every iteration of the MSF calculation loop. Otherwise, just calculate the MSF
+        % Define the intensity variable in the 1st and 4th quadrants
+        % Dimensions: colX = steps/2, x-range from qx_Q1_Range(1) ~ 0 to qx_Q1_Range(end) = +qMax
+        %             rowY = steps, y-range from range_qy(1) = -qMax to range_qy(end) = +qMax
+        intensity_Q1Q4 = zeros(steps,steps/2);
+        % Remove all NaN magnets
+        xR = xR(~isnan(xSpin)); yR = yR(~isnan(xSpin));
+        xSpin = xSpin(~isnan(xSpin)); ySpin = ySpin(~isnan(xSpin));
+        invSpinLength = 1/length(xSpin);
+
+        % Calculating MSF by calling subfunctions fullMSF or halfMSF
         if exist("app",'var')
             msfDialog = uiprogressdlg(app.IceScannerUI,'Title','Magnetic structure factor','Message',...
                 'Generating the magnetic structure factor. This may take several minutes.');
         end
-        
-        % Calculate MSF
-        for x = 1:steps
-            qx = range_qx(x);
-            q_dot_r_xComp = (qx*xR); 
-            % Determine the scattering unit vector
-            qNorm = sqrt((qx^2*onesVector_Q')+range_qy.^2);
-            qHat_x = qx./qNorm;
-            qHat_y = range_qy./qNorm; 
-            % Define the perpendicular spin vector (at the specified q) using matrix representation
-            q_dot_S = qHat_x.*xSpin + qHat_y.*ySpin;
-            q_dot_S_dot_qHat_x = q_dot_S.*qHat_x;
-            q_dot_S_dot_qHat_y = q_dot_S.*qHat_y;
-            % Define dot product vector between q and r_ij
-            q_dot_r = (onesVector_Q'.*q_dot_r_xComp + range_qy.*yR);
-            % Calculate A and B
-            A = [sum((xSpin - q_dot_S_dot_qHat_x).*cos(q_dot_r),1);sum((ySpin - q_dot_S_dot_qHat_y).*cos(q_dot_r),1)];
-            B = [sum((xSpin - q_dot_S_dot_qHat_x).*sin(q_dot_r),1);sum((ySpin - q_dot_S_dot_qHat_y).*sin(q_dot_r),1)];
-            MSF(x,:) = invSpinLength * (sum(A.*A,1) + sum(B.*B,1));
-            if exist("app",'var'); msfDialog.Value = x/steps; end
+        switch method
+            case 'full'; fullMSF;
+            case 'half'; halfMSF;
         end
-        if exist("app",'var'); close(msfDialog); end
 
-        %% Generate MSF figure
+        % Generate MSF figure
         if exist("app",'var')
             imageStatus = uiprogressdlg(app.IceScannerUI,'Title','Generating image','Message',...
                 'Generating MSF image for previewing.','Indeterminate','on');
@@ -73,9 +76,7 @@ function [MSF,MSFTable] = generateMSF(app,steps,qMax,xSpin,ySpin,xR,yR,options)
             print(msfFigure,sprintf('%sMSF.tif',options.SaveTo),'-dtiffn');
             close(msfFigure);
         end
-        if exist("app",'var')
-            close(imageStatus);
-        end
+        if exist("app",'var'); close(imageStatus); end
         
         % Converts 2D MSF matrix into n*3 matrix, where each column represents qx, qy, and I, respectively
         if exist("app",'var')
@@ -104,5 +105,65 @@ function [MSF,MSFTable] = generateMSF(app,steps,qMax,xSpin,ySpin,xR,yR,options)
         if exist('msfDialog','var'); close(msfDialog); end
         if exist("app",'var'); errorNotice(app,ME); end
         return;
+    end
+
+    %% Functions for calculating the "full" and "half" MSFs
+    % Calculate the MSF intensity at each point in q-space
+    function fullMSF
+        % Calculate MSF using inversion-symmetry-based calculation
+        onesVector_Q = ones(steps,1);
+        for x = 1:steps
+            qx = range_qx(x);
+            q_dot_r_xComp = (qx*xR); 
+            % Determine the scattering unit vector
+            qNorm = sqrt((qx^2*onesVector_Q')+range_qy.^2);
+            qHat_x = qx./qNorm;
+            qHat_y = range_qy./qNorm; 
+            % Define the perpendicular spin vector (at the specified q) using matrix representation
+            q_dot_S = qHat_x.*xSpin + qHat_y.*ySpin;
+            q_dot_S_dot_qHat_x = q_dot_S.*qHat_x; q_dot_S_dot_qHat_y = q_dot_S.*qHat_y;
+            % Define dot product vector between q and r_ij
+            q_dot_r = (onesVector_Q'.*q_dot_r_xComp + range_qy.*yR);
+            % Calculate A and B
+            A = [sum((xSpin - q_dot_S_dot_qHat_x).*cos(q_dot_r),1);sum((ySpin - q_dot_S_dot_qHat_y).*cos(q_dot_r),1)];
+            B = [sum((xSpin - q_dot_S_dot_qHat_x).*sin(q_dot_r),1);sum((ySpin - q_dot_S_dot_qHat_y).*sin(q_dot_r),1)];
+            MSF(:,x) = invSpinLength * (sum(A.*A,1) + sum(B.*B,1))';
+            if exist("app",'var'); msfDialog.Value = x/doubleSteps; end
+        end
+        if exist("app",'var'); close(msfDialog); end
+    end
+    % Symmetry-optimized MSF calculation: Solve for MSF intensity at each point
+    % in the 1st and 4th quadrants in q-space. Then use inversion symmetry
+    % to relate 1st-3rd and 4th-2nd quadrant intensities.
+    function halfMSF
+        for x = 1:length(qx_Q1_Range)
+            qx = qx_Q1_Range(x);
+            qx_xR = qx*xR;
+            for y = 1:length(range_qy)
+                % Determine the scattering vector
+                qy = range_qy(y);
+                qNorm = sqrt(qx^2+qy^2);
+                qHat_x = qx/qNorm;
+                qHat_y = qy/qNorm;
+                % Define the perpendicular spin vector (at the specified qx and qy) using matrix representation
+                q_dot_S = qHat_x*xSpin + qHat_y*ySpin;
+                S_perp_x = (xSpin - q_dot_S*qHat_x)';
+                S_perp_y = (ySpin - q_dot_S*qHat_y)';
+                % Define dot product vector between q=(qx,qy) and r_ij
+                % q_dot_r dimension: spinLength X 1 vector
+                q_dot_r = (qx_xR + qy*yR);
+                V_cos = cos(q_dot_r);
+                V_sin = sin(q_dot_r);
+                % Determine A and B
+                A = [S_perp_x*V_cos;S_perp_y*V_cos]; B = [S_perp_x*V_sin;S_perp_y*V_sin];
+                % Determine A and B (see Ostman's paper: https://doi.org/10.1038/s41567-017-0027-2)
+                intensity_Q1Q4(y,x) = invSpinLength * ((A'*A) + (B'*B));
+            end
+            if exist("app",'var'); msfDialog.Value = x/doubleSteps*2; end
+        end
+        if exist("app",'var'); close(msfDialog); end
+        % Update MSF
+        MSF(:,steps/2+1:end) = intensity_Q1Q4;
+        MSF(end:-1:1,steps/2:-1:1) = intensity_Q1Q4;
     end
 end
